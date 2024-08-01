88c648fb05fcfbced8227c43bea06e0fb7781663
Prajwal Luitel <prajwalluitel@gmail.com>
2024-08-01 15:01:36 -0400

    minor adjustments

--- frontend/talk-to-database-frontend/src/app/connected/page.js
+++ frontend/talk-to-database-frontend/src/app/connected/page.js
@@ -75,18 +75,17 @@ const Connected = () => {
         const text = await fetchText(sessionId);
         setConvertedText(text);
         console.log("Received the uploaded text: ", text)
-        const data = await processAudio(sessionId, text);
-        setProcessedData(data.dataframe);
       } else {
         alert("Error uploading audio file.");
       }
     }
   };
 
-  const testFinal = async () => {
-    const data = await processAudio("dd49482d-9ddd-4fdd-98ff-8fcfc159b245", "Give me the name of 3 employees.");
+  const processText = async () => {
+    const data = await processAudio(sessionId, "Give me the name of 3 employees.");
+    console.log("I received this from the final api call: ", data);
+    console.log("This is data.dataframe: ", data.dataframe);
     setProcessedData(data.dataframe);
-
   }
   
   
@@ -96,30 +95,30 @@ const Connected = () => {
       {sessionId ? (
         <div className="max-w-[75%] max-md:max-w-full m-auto text-center bg-emerald-950 rounded-xl pb-10">
           <h1 className="text-[2.5rem] pt-10 pb-8">
-            Database Connection Successful !
+            Connection Established with the Database Server
           </h1>
-
-          <div>
+          <h2>Talk to the Data : </h2>
+          <div className="w-full text-center">
             <button onClick={handleRecording}>
               {recording ? (
-                <div className="bg-rose-400 p-4 rounded-xl mb-4 text-center transition-all duration-1000 ease-in text-slate-900">
+                <div className="bg-rose-400 m-auto p-2 rounded-xl mb-4 text-center transition-all duration-1000 ease-in text-slate-900">
                   <Image
                     src="/Images/recording_stop.png"
                     width={50}
                     height={50}
                     alt="Start Recording"
-                    className="self-center w-[90%] m-auto"
+                    className="w-[40%] m-auto h-auto"
                   />
                   Stop Recording
                 </div>
               ) : (
-                <div className="bg-emerald-500 p-4 rounded-xl mb-4 text-center transition-all duration-1000 ease-in text-slate-900">
+                <div className="bg-emerald-500 m-auto p-2 rounded-xl mb-4 text-center transition-all duration-1000 ease-in text-slate-900">
                   <Image
                     src="/Images/recording_start.png"
                     width={50}
                     height={50}
                     alt="Start Recording"
-                    className="self-center w-[90%] m-auto"
+                    className="w-[40%] m-auto h-auto"
                   />
                   Start Recording
                 </div>
@@ -138,7 +137,7 @@ const Connected = () => {
 }
           </form>
 
-          <button onClick={testFinal} >Test final</button>
+          <button onClick={processText} className="bg-slate-300 p-4 rounded-lg mt-10 text-slate-800" >Process the Test</button>
 
           {uploaded && (
             <div>
@@ -156,7 +155,7 @@ const Connected = () => {
           {processedData && (
             <div>
               <p>Processed Data:</p>
-              <DataTable data={JSON.stringify(processedData) } />
+              <DataTable data={processedData} />
             </div>
           )}
         </div>
